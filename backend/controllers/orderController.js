8e72669b744fda58c77bb5cd67c17ddd47986153
Mabrook-Alizai <mabkalizai@gmail.com>
2025-01-10 22:31:04 +0500

    2

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,8 +1,8 @@
 import orderModel from "../models/orderModel.js";
 import userModel from "../models/userModel.js";
-import Stripe from "stripe";
+//import Stripe from "stripe";
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
+//const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 // placing user order from frontend
 const placeOrder = async (req, res) => {
@@ -42,12 +42,12 @@ const placeOrder = async (req, res) => {
             quantity:1
         })
 
-const session = await stripe.checkout.sessions.create({
+/*const session = await stripe.checkout.sessions.create({
     line_items : line_items,
     mode: 'payment',
     success_url: `${frontend_url}/verify?success=true&orderId=${newOrder._id}`,
     cancel_url: `${frontend_url}/verify?success=false&orderId=${newOrder._id}`
-})
+})*/
 
     res.json({success:true, session_url:session.url})
 
